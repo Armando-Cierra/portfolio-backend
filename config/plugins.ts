ad95690ff3d5104fd14be7ae94be0f7378e087af
Armando-Cierra <armando_cierra@outlook.com>
2023-12-27 22:51:08 -0600

    ✨ setting sendgrid and cloudinary as providers

---
+++ config/plugins.ts
@@ -0,0 +1,33 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_EMAIL_FROM"),
+        defaultReplyTo: env("SENDGRID_EMAIL_TO"),
+      },
+    },
+  },
+});
